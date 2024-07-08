c3aa3d19a695cb3afb4f831157e41cff3728a567
David Lim <beitrik2014@gmail.com>
2024-07-08 16:39:18 +0800

    Input string (string to be encoded) no longer hardcoded; comes from User Input

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length < 2)
+        {
+            System.out.println("Insufficient arguments. Required: <alphabet> <offset>.");
+            System.exit(-1);
+        }
+
         IEncoder encoder = new TextEncoder(args[0]);;
 
         try
         {
-            String strOriginal = "HELLO WORLD";
-            System.out.println("Original : " + strOriginal);
+            System.out.println("Please enter the text you want to encode/decode.");
+            String strOriginal = new Scanner(System.in).nextLine();
 
             List encodingInput = new ArrayList<String>();
             encodingInput.add(strOriginal);
